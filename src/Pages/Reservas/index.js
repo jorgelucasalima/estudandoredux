de8ca58eecff9273ac80a3c687d0154d380636f9
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-20 14:59:27 -0300

    estilizado a pages de reservas

--- src/Pages/Reservas/index.js
+++ src/Pages/Reservas/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {FaTrash} from 'react-icons/fa'
+import './style.css'
+
+
 
 export default function Reservas() {
  return (
    <div>
-     <h1>Reservas</h1>
+      <h1 className="title">Você solicitou 1 reservas</h1>
+      <div className="reservas">
+        <img 
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" 
+          alt="Maceio" />
+          <strong>Viagem Maceio 7 dias</strong>
+          <span>Quantidade: 2</span>
+          <button 
+            type="button"
+            onClick={()=>{}}
+            >
+              <FaTrash size={20} color="#FFF" />
+            </button>
+      </div>
+
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
+
+
    </div>
  );
 }
