cd369fe9c83cc6e5b45b06bc50595ed9e02102c0
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-20 16:26:01 -0300

    instalado redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import react from "react";
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './routes'
+import { Provider } from "react-redux";
+import store from "./Store";
 
 import Header from "./Components/Header";
 
 export default function App(){
   return(
-    <BrowserRouter>
-      <Header/>
-      <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header/>
+        <Routes/>
+      </BrowserRouter>  
+    </Provider>
   )
 }
