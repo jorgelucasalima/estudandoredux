0069cbd76d9327f41758eda0b2b6d7de9aa2d8c0
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-20 13:54:52 -0300

    instaldo axios para consulta na api de teste

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../../services/api';
 
 export default function Home() {
- return (
+ 
+  const [trips, setTrips] = useState([]);
+ 
+  useEffect(() => {
+    
+    async function loadApi() {
+      const response = await api.get('trips')
+      setTrips(response.data)
+    }
+
+    loadApi()
+
+  }, []);
+ 
+  return (
    <div>
      <h1>
        Home
