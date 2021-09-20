832962861a5477182c8cfee054bd2a6f45ad2036
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-20 13:35:07 -0300

    realizado estilização da Home

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/logo.svg'
+import './style.css'
+
+
 
 export default function Header() {
  return (
-   <header>
-     <h1>Header</h1>
+   <header className="container">
+     
+     <Link to="/">
+        <img className="logo" src={logo} alt="Logo" /> 
+     </Link>
+
+     <Link className="reserva" to="reservas">
+       <div>
+         <strong>Minhas Reservas</strong>
+         <span>0 Reservas</span>
+       </div>
+     </Link>
 
    </header>
  );
