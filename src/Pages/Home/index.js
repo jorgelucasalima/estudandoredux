95b0dbeb256596f3fae27ba70f84cbf4eefa6fd7
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-20 14:44:21 -0300

    estilizado home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import api from '../../services/api';
+import {FaPlane} from 'react-icons/fa'
+import './style.css'
 
 export default function Home() {
  
@@ -10,17 +12,37 @@ export default function Home() {
     async function loadApi() {
       const response = await api.get('trips')
       setTrips(response.data)
+    
+    
+      console.log(response.data)
     }
 
     loadApi()
+    
 
   }, []);
  
   return (
    <div>
-     <h1>
-       Home
-     </h1>
+     <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+              <strong>{trip.title}</strong>
+              <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel' }</span>
+
+              <button
+                type="button"
+                onClick={()=>{}}
+              >
+                <div>
+                  <FaPlane size={16} color="#FFF"/>
+                </div>
+                <span>Solicitar Reserva</span>
+              </button>
+          </li>
+        ))} 
+     </div>
    </div>
  );
 }
